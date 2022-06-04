fc747bae24805efe5d1e9a3648b487339acca078
David Demerchyan <davidemer11@yandex.ru>
2022-06-04 15:56:54 +0300

    Предварительный. Суббота. 16:00

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,31 @@
+from email.policy import default
+from attr import field
+from rest_framework import serializers
+from reviews.models import Review, Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    review = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
