3b86b0aa69c6bd8649b2407999f7848bb4a3fe1e
David Demerchyan <davidemer11@yandex.ru>
2022-06-07 19:47:06 +0300

    еще поправил

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -11,9 +11,13 @@ class User(AbstractUser):
     )
     first_name = models.CharField(_('first name'), max_length=150, blank=True)
     role = models.CharField(max_length=256, choices=ROLES, default=ROLES[0][0])
-    email = models.EmailField(_('email address'), blank=False, unique=True, max_length=254)
+    email = models.EmailField(
+        _('email address'), blank=False, unique=True, max_length=254
+    )
     bio = models.TextField(_('biography'), blank=True,)
-    confirmation_code = models.IntegerField(_('confirmation code'), blank=True, null=True)
+    confirmation_code = models.IntegerField(
+        _('confirmation code'), blank=True, null=True
+    )
 
     def __str__(self):
         return self.username
