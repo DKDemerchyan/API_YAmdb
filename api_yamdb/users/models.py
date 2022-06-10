3f28e529d599ed3bf97e96d198722c7219fe37d0
David Demerchyan <davidemer11@yandex.ru>
2022-06-10 18:57:36 +0300

    второе ревью

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -4,20 +4,31 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
     ROLES = (
         ('user', 'User'),
         ('moderator', 'Moderator'),
         ('admin', 'Administrator'),
     )
-    first_name = models.CharField(_('first name'), max_length=150, blank=True)
-    role = models.CharField(max_length=256, choices=ROLES, default=ROLES[0][0])
-    email = models.EmailField(
-        _('email address'), blank=False, unique=True, max_length=254
+    role = models.CharField(
+        max_length=256,
+        choices=ROLES,
+        default=ROLES[0][0],
     )
-    bio = models.TextField(_('biography'), blank=True,)
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    email = models.EmailField(_('email address'), blank=False, unique=True)
     confirmation_code = models.IntegerField(
-        _('confirmation code'), blank=True, null=True
-    )
+        _('confirmation_code'), blank=True, null=True)
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
 
-    def __str__(self):
-        return self.username
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
